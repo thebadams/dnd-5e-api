f7e49a99b1bca1de82a090f9ef7ea200df516c48
Brian Adams <bpeteradams@gmail.com>
2021-11-11 18:40:49 -0500

    wrote first test checking for an _id property on the Spell Schema

---
+++ test/Spell.test.js
@@ -0,0 +1,26 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongoose = require('mongoose');
+const Spell = require('../models/Spell');
+
+describe('Spell Schema', () => {
+  let mongod
+  beforeAll(async () => {
+    mongod = await MongoMemoryServer.create()
+    const uri = mongod.getUri()
+    await mongoose.connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }).then(() => console.log('Connected To Database'))
+  })
+
+  afterAll(async () => {
+    await mongoose.disconnect().then(() => console.log('Database Disconnected'))
+    await mongod.stop().then(() => console.log('Database Torn Down'))
+  })
+  describe('Spell Schema Properties', () => {
+    it('Should Have An _id Property', async () => {
+      const newSpell = await Spell.create({})
+      expect(newSpell).toHaveProperty('_id')
+    })
+  })
+})
