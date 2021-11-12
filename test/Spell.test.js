a15f5699e5a3ae63f2a545a402740b894589d17b
Brian Adams <bpeteradams@gmail.com>
2021-11-11 19:50:50 -0500

    Wrote test.todos for all properties on the Spell Schema

--- test/Spell.test.js
+++ test/Spell.test.js
@@ -20,10 +20,73 @@ describe('Spell Schema', () => {
   describe('Spell Schema Properties', () => {
     let newSpell
     beforeAll(async () => {
-      newSpell = await Spell.create({})
+      const components = {
+        v: true,
+        s: true,
+        m: 'a tiny ball of bat guano and sulfur'
+      }
+      const description = {
+        main: `A Ball of Fire Appears, Doing Fire Damage`,
+        atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
+      }
+      const spellInfo = {
+        name: 'Fireball',
+        level: 3,
+        school: 'Evocation',
+        castingTime: '1 Action',
+        components,
+        duration: 'Instantaneous',
+        description
+      }
+      newSpell = await Spell.create(spellInfo)
+      console.log(newSpell)
     })
-    it('Should Have An _id Property', async () => {
+    it('Should Have An _id Property', () => {
       expect(newSpell).toHaveProperty('_id')
     })
+    describe('Name Property', () => {
+      it('Should Have A "name" Property', () => {
+      expect(newSpell).toHaveProperty('name')
+    });
+      it('Should Be Fireball', () => {
+        expect(typeof newSpell.name).toBe('string')
+        expect(newSpell.name).toBe('Fireball');
+      });
+    });
+    describe('Level Property', () => {
+      test.todo('Should Have A "level" Property');
+      test.todo('Should Be The Number "3"');
+    });
+    describe('School Property', () => {
+      test.todo('Should Have A "school" Property');
+      test.todo('Should Be The String, "Evocation"');
+    });
+    describe('Casting Time Property', () => {
+      test.todo('Should Have A "castingTime" Property');
+      test.todo('Should Be The String, "1 Action"');
+    });
+    describe('Range Property', () => {
+      test.todo('Should Have A "range" Property');
+      test.todo('Should Be The String, "150 feet"')
+
+    });
+    describe('Components Property', () => {
+      test.todo('Should Have A "components Property"')
+      test.todo('Should Be An Object With "v", "s", "m" Properties')
+      test.todo('Component "v" Property Should Be "true" Boolean');
+      test.todo('Component "s" Property Should Be "true" Boolean');
+      test.todo('Component "m" Property Should Be String "a tiny ball of bat guano and sulfur"');
+    });
+    describe('Duration Property', () => {
+      test.todo('Should Have A "duration" Property');
+      test.todo('Should Be The String, "Instantaneous"');
+    });
+    describe('Description Property', () => {
+      test.todo('Should Have A "description" Property');
+      test.todo('Should Be An Object With "main" and "atHigherLevels" Properties');
+      test.todo('Description "main" Property Should Be The String "A Ball of Fire Appears, Doing Fire Damage"')
+      test.todo('Description "atHigherLevels" Property Should Be The String "When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd."')
+    })
+    
   })
 })
