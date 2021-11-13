eed05af9681a0c2251f0b8b5dbcad50987d07d43
bpeteradams <bpeteradams@gmail.com>
2021-11-13 09:26:57 -0500

    Added back in schoolsTuple for use with enum in model validation on the Spell schema model

--- src/constants/schools.ts
+++ src/constants/schools.ts
@@ -1,6 +1,8 @@
-//  const schools: [string, string, string, string, string, string, string, string] = ['Conjuration', 'Evocation', 'Necromancy', 'Abjuration', 'Transmutation', 'Divination', 'Enchantment', 'Illusion'];
-
- enum Schools { EVOCATION = 'Evocation',
+ let schoolsTuple: [string, string, string, string, string, string, string, string];
+schoolsTuple = ['Conjuration', 'Evocation', 'Necromancy', 'Abjuration', 'Transmutation', 'Divination', 'Enchantment', 'Illusion'];
+ 
+ enum SchoolsEnum { EVOCATION = 'Evocation',
 CONJURATION = 'Conjuration', NECROMANCY = 'Necromancy', ABJURATION = 'Abjuration', TRANSMUTATION = 'Transmutation', DIVINATION = 'Divination', ENCHANTMENT = 'Enchantment', ILLUSION = 'Illusion' }
 
- export default Schools;
+
+export { SchoolsEnum, schoolsTuple };
--- src/tests/constants/schools.test.ts
+++ src/tests/constants/schools.test.ts
@@ -1,45 +1,47 @@
-import Schools from '../../constants/schools';
+import {SchoolsEnum, schoolsTuple} from '../../constants/schools';
 
 describe('Schools Enum', () => {
-  console.log(Schools)
+  console.log(SchoolsEnum)
   test('Schools Should Be An Enum', () => {
-    expect(typeof Schools).toBe('object');
+    expect(typeof SchoolsEnum).toBe('object');
   })
   test('Schools Should Have Property "EVOCATION" That Has The Value, "Evocation"', () => {
-    expect(Schools).toHaveProperty('EVOCATION', 'Evocation');
+    expect(SchoolsEnum).toHaveProperty('EVOCATION', 'Evocation');
   })
   test('Schools Should Have Property "CONJURATION" That Has The Value, "Conjuration"', () => {
-    expect(Schools).toHaveProperty('CONJURATION', 'Conjuration');
+    expect(SchoolsEnum).toHaveProperty('CONJURATION', 'Conjuration');
   })
   test('Schools Should Have Property "NECROMANCY" That Has Value, "Necromancy"', () => {
-    expect(Schools).toHaveProperty('NECROMANCY', 'Necromancy');
+    expect(SchoolsEnum).toHaveProperty('NECROMANCY', 'Necromancy');
   })
   test('Schools Should Have Property "ABJURATION" That Has Value, "Abjuration"', () => {
-    expect(Schools).toHaveProperty('ABJURATION', 'Abjuration');
+    expect(SchoolsEnum).toHaveProperty('ABJURATION', 'Abjuration');
   });
   test('Schools Should Have Property "TRANSMUTATION" That Has Value, "Transmutation"', () => {
-    expect(Schools).toHaveProperty('TRANSMUTATION', 'Transmutation');
+    expect(SchoolsEnum).toHaveProperty('TRANSMUTATION', 'Transmutation');
   });
   test('Schools Should Have Property "DIVINATION" That Has Value, "Divination"', () => {
-    expect(Schools).toHaveProperty('DIVINATION', 'Divination');
+    expect(SchoolsEnum).toHaveProperty('DIVINATION', 'Divination');
   });
   test('Schools Should Have Property "ENCHANTMENT" That Has Value, "Enchantment"', () => {
-    expect(Schools).toHaveProperty('ENCHANTMENT', 'Enchantment');
+    expect(SchoolsEnum).toHaveProperty('ENCHANTMENT', 'Enchantment');
   });
   test('Schools Should Have Property "ILLUSION" That Has Value, "Illusion"', () => {
-    expect(Schools).toHaveProperty('ILLUSION', 'Illusion');
+    expect(SchoolsEnum).toHaveProperty('ILLUSION', 'Illusion');
   })
+})
 
-  // test('schools should be an Array', () => {
-  //   expect(Array.isArray(schools)).toBeTruthy();
-  // })
-  // test('schools should be an array of 8 length', () => {
-  //   expect(schools.length).toBe(8);
-  // })
-  // test('schools should have the expected list of school types', () => {
-  //   const expectedArray = ['Conjuration', 'Evocation', 'Necromancy', 'Abjuration', 'Transmutation', 'Divination', 'Enchantment', 'Illusion'];
+describe('Schools Tuple', () => {
+  test('schools should be an Array', () => {
+    expect(Array.isArray(schoolsTuple)).toBeTruthy();
+  })
+  test('schools should be an array of 8 length', () => {
+    expect(schoolsTuple.length).toBe(8);
+  })
+  test('schools should have the expected list of school types', () => {
+    const expectedArray = ['Conjuration', 'Evocation', 'Necromancy', 'Abjuration', 'Transmutation', 'Divination', 'Enchantment', 'Illusion'];
 
-  //   expect(schools).toEqual(expect.arrayContaining(expectedArray));
+    expect(schoolsTuple).toEqual(expect.arrayContaining(expectedArray));
 
-  // })
+  })
 })
