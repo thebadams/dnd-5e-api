aa214a9fada4bdf54c70d8dc07d1eda9b1ee23fc
TheBadams <bpeteradams@gmail.com>
2021-11-15 11:27:50 -0500

    Wrote a spell level tuple and enum

---
+++ src/constants/levels.ts
@@ -0,0 +1,14 @@
+export const levelTuple: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+export enum levelEnum {
+	CANTRIP = 0,
+	ONE,
+	TWO,
+	THREE,
+	FOUR,
+	FIVE,
+	SIX,
+	SEVEN,
+	EIGHT,
+	NINE
+}
