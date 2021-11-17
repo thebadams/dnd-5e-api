412b9f09e58ef422e7a875c2dbc6ff9a393b6568
Brian Adams <bpeteradams@gmail.com>
2021-11-16 22:08:24 -0500

    Wrote requirement test for castingTime property on the Spell Schema

--- src/tests/models/Spell.test.ts
+++ src/tests/models/Spell.test.ts
@@ -347,8 +347,53 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Casting Time Validation', () => {
-			test.todo('Casting Time Is Required; If None Is Provided, Throw An error,');
-			test.todo('If No Casting Time Is Provided, Throw a Validation Error');
+			let badSpell: mongoose.Error.ValidationError;
+			beforeAll(() => {
+				beforeAll(async () => {
+					const components: IComponents = {
+						v: true,
+						s: true,
+						m: 'bat guano and sulfur'
+					};
+
+					const description: IDescription = {
+						main: 'A Ball of Fire Appears, Doing Fire Damage',
+						atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
+					};
+					const badSpellInfo = {
+						name: 'Fireball',
+						level: 3,
+						school: 'Evocation',
+						//castingTime: '1 Action',
+						range: '150 Ft',
+						components,
+						duration: 'Instantaneous',
+						description
+					};
+
+
+					badSpell = await Spell.create(badSpellInfo).catch((e) => e);
+					//console.log(badSpellLevel.errors);
+					//console.log(badSpell);
+
+
+				});
+			});
+			test('Casting Time Is Required; If None Is Provided, Throw An error', () => {
+				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
+			});
+			test('If No Casting Time Is Provided, Message Should Be "Casting Time Must Be Provided"', () => {
+				const expected = {
+					castingTime: expect.objectContaining({
+						properties: expect.objectContaining({ message: 'Casting Time Must Be Provided', type: 'required', path: 'castingTime' }
+						),
+						kind: 'required',
+						path: 'castingTime'
+					})
+
+				};
+				expect(badSpell).toHaveProperty('errors', expect.objectContaining(expected));
+			});
 		});
 		describe('Range Validation', () => {
 			test.todo('Range Is Required; If None Is Passed In Expect An Error');
