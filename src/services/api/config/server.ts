0ab5692529fda37f9941d9399e8f3004c8f25023
TheBadams <bpeteradams@gmail.com>
2021-11-20 14:55:36 -0500

    Added PORT to configuration file of server.ts

--- src/services/api/config/server.ts
+++ src/services/api/config/server.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import setUpBodyParser from '../../../utils/express/setupBodyParser';
 import setupURLEncoded from '../../../utils/express/setupURLEncoded';
 
+export const PORT = 3001;
 const app = express();
 
 setUpBodyParser(app);
 setupURLEncoded(app, { extended: true });
 
 export default app;
+
