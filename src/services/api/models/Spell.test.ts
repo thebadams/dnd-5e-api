fbdb68f5c9691f78f96c017aecfea2591565216c
TheBadams <bpeteradams@gmail.com>
2021-11-20 16:55:57 -0500

    made adjustments to the spell.test suite

--- src/services/api/models/Spell.test.ts
+++ src/services/api/models/Spell.test.ts
@@ -165,36 +165,36 @@ describe('Spell Schema', () => {
     
 	});
 	describe('Spell Model Validation', () => {
+		let badSpell: mongoose.Error.ValidationError;
+		let badSpellOptions: mongoose.Error.ValidationError;
+		beforeAll(async () => {
+			const components: IComponents = {
+				v: true,
+				s: true,
+				m: 'bat guano and sulfur'
+			};
+
+			const description: IDescription = {
+				main: 'A Ball of Fire Appears, Doing Fire Damage',
+				atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
+			};
+			const badSpellInfo = {
+				level: 10,
+				school: 'Evo',
+				castingTime: '1 Action',
+				range: '150 Ft',
+				components,
+				duration: 'Instantaneous',
+				description,
+				concentration: false,
+				ritual: false
+			};
+
+			badSpell = await Spell.create({}).catch((e) => e);
+			badSpellOptions = await Spell.create(badSpellInfo).catch((e) => e);
+
+		});
 		describe('Name Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components : IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description : IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description,
-					concentration: false,
-					ritual: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				
-				
-			});
 			test('Spell Name Is Required; If None Is Passed In, Throw Error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -213,43 +213,6 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Level Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			let badSpellLevel: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					// level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description,
-					concentration: false,
-					ritual: false
-				};
-				const badSpellLevelInfo = {
-					...badSpellInfo, level: 10
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				badSpellLevel = await Spell.create(badSpellLevelInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Spell Level Is Required; If One Is Not Passed In And Error Should Be Thrown', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -266,7 +229,7 @@ describe('Spell Schema', () => {
 				expect(badSpell).toHaveProperty('errors', expect.objectContaining(expected));
 			});
 			test('If A Spell Level, Not Of Numbers 0-9 Are Passed In, Throw A Validation Error', () => {
-				expect(badSpellLevel).toBeInstanceOf(mongoose.Error.ValidationError);
+				expect(badSpellOptions).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
 			test('If A Spell Level Error Is Thrown, Message Should Be "10 Is Not A Valid Spell Level"', () => {
 				const expected = {
@@ -280,47 +243,10 @@ describe('Spell Schema', () => {
 						path: 'level'
 					})
 				};
-				expect(badSpellLevel).toHaveProperty('errors', expect.objectContaining(expected));
+				expect(badSpellOptions).toHaveProperty('errors', expect.objectContaining(expected));
 			});
 		});
 		describe('School Validation', () => {
-			let badSpell : mongoose.Error.ValidationError;
-			let badSpellSchool: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					//school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description,
-					ritual: false,
-					concentration: false
-				};
-				const badSpellSchoolInfo = {
-					...badSpellInfo, school: 'Evo'
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				badSpellSchool = await Spell.create(badSpellSchoolInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('School Is Required, If None Is Passed In, Throw An Error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -337,7 +263,7 @@ describe('Spell Schema', () => {
 				expect(badSpell).toHaveProperty('errors', expect.objectContaining(expected));
 			});
 			test('If A Wrong Spell School Is Provided, Throw A Validation Error', () => {
-				expect(badSpellSchool).toBeInstanceOf(mongoose.Error.ValidationError);
+				expect(badSpellOptions).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
 			test('If School Is Provided Value, "Evo", Message Should Be "Evo Is Not A Valid Spell School"', () => {
 				const expected = {
@@ -349,40 +275,10 @@ describe('Spell Schema', () => {
 					})
 
 				};
-				expect(badSpellSchool).toHaveProperty('errors', expect.objectContaining(expected));
+				expect(badSpellOptions).toHaveProperty('errors', expect.objectContaining(expected));
 			});
 		});
 		describe('Casting Time Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					//castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Casting Time Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -400,36 +296,6 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Range Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					//range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Range Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -446,39 +312,7 @@ describe('Spell Schema', () => {
 				expect(badSpell).toHaveProperty('errors', expect.objectContaining(expected));
 			});
 		});
-		describe('Range Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				// const components: IComponents = {
-				// 	v: true,
-				// 	s: true,
-				// 	m: 'bat guano and sulfur'
-				// };
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					//components,
-					duration: 'Instantaneous',
-					description,
-					concentration: false,
-					ritual: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
+		describe('Components Validation', () => {
 			test('Range Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -496,38 +330,6 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Duration Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					//duration: 'Instantaneous',
-					description,
-					concentration: false,
-					ritual: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Duration Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -545,38 +347,6 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Description Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				// const description: IDescription = {
-				// 	main: 'A Ball of Fire Appears, Doing Fire Damage',
-				// 	atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				// };
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					//description,
-					ritual: false,
-					concentration: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Description Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -594,38 +364,6 @@ describe('Spell Schema', () => {
 			});
 		});
 		describe('Concentration Validation', () => {
-			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description,
-					//concentration: false,
-					ritual: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Concentration Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
@@ -644,37 +382,6 @@ describe('Spell Schema', () => {
 		});
 		describe('Ritual Validation', () => {
 			let badSpell: mongoose.Error.ValidationError;
-			beforeAll(async () => {
-				const components: IComponents = {
-					v: true,
-					s: true,
-					m: 'bat guano and sulfur'
-				};
-
-				const description: IDescription = {
-					main: 'A Ball of Fire Appears, Doing Fire Damage',
-					atHigherLevels: 'When you cast this spell using a spell slot of 4th level or higher, the damage increases by 1d6 for each slot level above 3rd.'
-				};
-				const badSpellInfo = {
-					name: 'Fireball',
-					level: 3,
-					school: 'Evocation',
-					castingTime: '1 Action',
-					range: '150 Ft',
-					components,
-					duration: 'Instantaneous',
-					description,
-					concentration: false,
-					//ritual: false
-				};
-
-
-				badSpell = await Spell.create(badSpellInfo).catch((e) => e);
-				//console.log(badSpellLevel.errors);
-				//console.log(badSpell);
-
-
-			});
 			test('Ritual Is Required; If None Is Provided, Throw An error', () => {
 				expect(badSpell).toBeInstanceOf(mongoose.Error.ValidationError);
 			});
